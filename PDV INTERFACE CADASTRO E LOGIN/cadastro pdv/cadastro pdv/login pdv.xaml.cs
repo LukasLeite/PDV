a844d9ec9a4a6b2c35acafca2ccac1e896d779b8
MatheusPrado0096 <matheusdprado179@gmail.com>
2025-05-28 20:15:38 -0300

    Add files via upload

---
+++ PDV INTERFACE CADASTRO E LOGIN/cadastro pdv/cadastro pdv/login pdv.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+
+namespace cadastro_pdv
+{
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            string id = IdTextBox.Text;
+            string senha = SenhaBox.Password;
+
+            // Exemplo simplificado (substituir por lógica real)
+            if (id == "12345678" && senha == "admin")
+            {
+                MessageBox.Show("Login bem-sucedido!");
+            }
+            else
+            {
+                MessageBox.Show("ID ou senha incorretos!");
+            }
+
+        }
+
+        private void AbrirCadastro_Click(object sender, RoutedEventArgs e)
+        {
+            CadastroWindow cadastro = new CadastroWindow();
+            cadastro.Show();
+            this.Close(); // fecha a tela de login
+        }
+    }
+}
